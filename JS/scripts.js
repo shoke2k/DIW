eb61bc76c57125474080ff671d5fe2357fbc6d07
Shoke 2K <shoke_2k@hotmail.com>
2020-02-03 19:37:03 +0100

    JScript ventana

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,6 @@
-//Cierra el menú al hacer click en un item
+//Cierra el menú al hacer click en un item si el ancho de la ventana es inferior a 768 pixeles
 $('.nav a').on('click', function(){
-    if (screen.width < 768){
+    if (window.innerWidth < 768){
         $('.navbar-toggler').click(); //bootstrap 4.x
     }
 });
